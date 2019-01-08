b20217eacdbf84b8887a488846ae0f4a0b42f6ec
frederic lehmann <fred@navis.sillage.local>
2019-01-08 15:06:55 +0100

    More comments

--- startstop.py
+++ startstop.py
@@ -28,7 +28,7 @@ def checkthem(actiontime):
     #First step : arret des serveurs qui tournent
     print ("Get Running EC2 Instances and stop them if necessary....")
     myec2instanceslist = stdexplib.get_instanceid_by_state("running",region)
-    get_check_actions("Name","StartDailyTime","StopDailyTime","OpeningDays",["i-01a9be017b577d7f8"],"stop",actiontime)
+    get_check_actions("Name","StartDailyTime","StopDailyTime","OpeningDays",["i-01a9be017b577d7f8"],"running",actiontime)
 
     myec2instanceslist = []
 
@@ -47,22 +47,18 @@ def checkthem(actiontime):
 #   instanceslist => EC2 instances list to check
 #   action => start or stop
 # Output : Nothing
-def get_check_actions(tag1,tag2,tag3,tag4,instanceslist,action,actiontime):
+def get_check_actions(tag1,tag2,tag3,tag4,instanceslist,state,actiontime):
 
     idtodel = []
     
-    if action == "start" :
-            state = "Stopped"
-    else :
-            state = "Running"
-
     taglist = [tag1,tag2,tag3,tag4]
     instancesdata = stdexplib.get_ec2tagsvalues(region,instanceslist,[tag1,tag2,tag3,tag4])
 
     for index in range(0, len(instancesdata), 1):
         print (instancesdata[index])
+        instanceid = instancesdata[index][0]
         if instancesdata[index][1] == "NO TAG":
-            InstanceName = instancesdata[index][0]
+            InstanceName = instanceid
         else:
             InstanceName = instancesdata[index][1]
 
@@ -82,26 +78,29 @@ def get_check_actions(tag1,tag2,tag3,tag4,instanceslist,action,actiontime):
             OpDays = instancesdata[index][4]
         
         # check the consistency of the values
-        if not(verify_time_format(StartTime)) or not(verify_time_format(StopTime)) or not(verify_days_format(OpDays)):
+        if not(stdexplib.verify_time_format(StartTime)) or not(stdexplib.verify_time_format(StopTime)) or not(stdexplib.verify_days_format(OpDays)):
             print (InstanceName+" - "+state+" - Tags missing or format ko : leaving in the actual state")
             idtodel.append(instanceid)
         # Check if we have something todo regarding the different values
         else:
             # if it's a working day
-            if check_day(OpDays):
-                if check_slot(StartTime,StopTime,action,actiontime) == 0:
+            if stdexplib.check_day(OpDays):
+                if stdexploib.check_slot(StartTime,StopTime,state,actiontime) == 0:
                     print (InstanceName+" - "+state+" - State OK")
                     idtodel.append(instanceid)
-                elif check_slot(StartTime,StopTime,action,actiontime) == 1:
-                    print (InstanceName+" - "+state+" - To "+action)
-                elif check_slot(StartTime,StopTime,action,actiontime) == 2:
-                    if action == "start":
+                elif stdexploib.check_slot(StartTime,StopTime,state,actiontime) == 1:
+                    if state == "Running":
+                        print (InstanceName+" - "+state+" - To Stop")
+                    else:
+                        print (InstanceName+" - "+state+" - To Start")
+                elif stdexploib.check_slot(StartTime,StopTime,state,actiontime) == 2:
+                    if state == "stopped":
                         print (InstanceName+" - "+state+" - To Start")
             # If it's not a working day
-            elif (not(check_day(OpDays))):
-                if action == "stop":
+            elif (not(stdexplib.check_day(OpDays))):
+                if state == "running":
                     print (InstanceName+" - "+state+" - To Stop")
-                elif action == "start":
+                elif state == "stopped":
                     print (InstanceName+" - "+state+" - State OK")
                     idtodel.append(instanceid)
             # Fall back
@@ -114,167 +113,9 @@ def get_check_actions(tag1,tag2,tag3,tag4,instanceslist,action,actiontime):
 
     if len(instanceslist) > 0:
         print (len(instanceslist)," instance(s) to ",action)
-        ec2instances_action(instanceslist,action)
+        stdexplib.ec2instances_action(instanceslist,action)
     else:
         print ("0 instance to ",action)
-
-
-# Check the correct configuration of a time data based on REGEXP
-# Input : StartDailyTime or StopDailyTime tag value
-# Output :
-#   True if formzt is ok
-#   False if format is ko
-#
-def verify_time_format(tagvalue):
-
-    import re
-
-    if re.match('^[0-9]{2}\:[0-9]{2}\:[0-9]{2}\+[0-9]{2}\:[0-9]{2}$',tagvalue):
-        if int(tagvalue[:2])==99:
-            return True
-        if (int(tagvalue[:2])>23) or (int(tagvalue[3:5])>59) or (int(tagvalue[6:8])>59):
-            print (tagvalue+" : Time Format OK - Data KO")
-            return False
-        else:
-            return True
-    else:
-        # print (tagvalue+" : Time Format KO")
-        return False
-
-#
-# Check the correct configuration of a day data based on REGEXP
-# Input : Openingays tag value
-# Output :
-#   True if formzt is ok
-#   False if format is ko
-#
-def verify_days_format(tagvalue):
-
-    import re
-
-    if re.match('^[aA-zZ]{3}(,[aA-zZ]{3})*$',tagvalue):
-        return True
-        print (tagvalue + " : OpeningDays Format KO")
-    else:
-        return False
-
-#
-# Check if today we must start the instances
-# Input : Openingays tag value
-# Output :
-#   True if it's a working day
-#   False if it's not a working day
-#
-def check_day(tagvalue):
-
-    from time import strftime
-    n = 0
-
-    actualday = strftime("%a").upper()
-
-    tagsplit=tagvalue.split(",")
-    for day in tagsplit:
-        if day == actualday:
-            n=1
-
-    if n == 1:
-        return True
-    else:
-        return False
-
-
-#
-# Check if it is the good time to do something
-# Input :
-#   StartTime
-#   StopTime
-#   State of the instance
-#
-# Output
-#   0 : do nothing
-#   1 : start or stop the instance
-#   2 : start and stop time are the same (but not 99)
-#
-def check_slot(tagvalue1,tagvalue2,state,actiontime):
-
-    #print ("StartTime : "+tagvalue1)
-    #print ("StopTime : "+tagvalue2)
-
-    if int(tagvalue1[:2]) == 99:
-        if int(tagvalue2[:2]) == 99:
-            return 0
-        else:
-            if state == "stop" and check_time(tagvalue2,actiontime) == 1:
-                return 1
-            else:
-                return 0
-    elif int(tagvalue2[:2]) == 99:
-        if int(tagvalue1[:2]) == 99:
-            return 0
-        else:
-            if state == "start" and check_time(tagvalue1,actiontime) == 1:
-                return 1
-            else:
-                return 0
-    elif (int(tagvalue1[:2]) != 99) and (int(tagvalue2[:2]) != 99) and (tagvalue1 == tagvalue2):
-        return 2
-    else:
-        if state == "start":
-            if check_time(tagvalue1,actiontime) == 1 and check_time(tagvalue2,actiontime) == 0:
-                return 1
-            else:
-                return 0
-        elif state == "stop":
-            if (check_time(tagvalue1,actiontime) == 1 and check_time(tagvalue2,actiontime) == 1) or (check_time(tagvalue1,actiontime) == 0 and check_time(tagvalue2,actiontime) == 0):
-                return 1
-            else:
-                return 0
-        else:
-            return 0
-
-#
-# Check if it is the good time to do something
-# Input : StartDailyTime or StopDailyTime tag value
-# Output : 0 if ok / 1 if ko / 2 if 99
-#
-def check_time(tagvalue,actiontime):
-
-    from datetime import time
-    
-    actualtime = time(int(actiontime[:2]), int(actiontime[3:5]), int(actiontime[6:8]))
-    idgmttime = time(int(tagvalue[:2]), int(tagvalue[3:5]), int(tagvalue[6:8]))
-    
-    #now = datetime.now()
-    #actualtime = now.time()
-    #if tagvalue[8:9] == "+":
-    #    idgmttime = time(int(tagvalue[:2])-int(tagvalue[9:11]), int(tagvalue[3:5]), int(tagvalue[6:8]))
-    #else:
-    #    idgmttime = time(int(tagvalue[:2])+int(tagvalue[9:11]), int(tagvalue[3:5]), int(tagvalue[6:8]))
-
-    if idgmttime <= actualtime:
-        return 1
-    else:
-        return 0
-
-#
-# Start or Stop EC2 instances
-# Input :
-#   instanceslist => list of EC2 instances ID
-#   action => action to perform on the instances in the list
-# Output : Nothing
-#
-def ec2instances_action(instanceslist,action):
-
-    ec2 = boto3.client('ec2', region_name=region)
-
-    if action == "start":
-        for id in instanceslist:
-            print (id+" starting")
-        #ec2.start_instances(InstanceIds=instanceslist)
-    else:
-        for id in instanceslist:
-            print (id+" stopping")
-        #ec2.stop_instances(InstanceIds=instanceslist)
 
 #
 # Main for Lambda
--- stdexplib.py
+++ stdexplib.py
@@ -9,6 +9,12 @@
 # Functions Summary
 #
 #	- get_instanceid_by_state(state) / get all the Running or Stoped instances ID
+#       - get_ec2tagsvalues(region,instanceslist,tagslist) / get some tags values from an instances ID list
+#       - verify_time_format(data) / check data format for time rekognition
+#       - verify_days_format(data) / check data format for days rekognition
+#       - check_slot(starttime,stoptime,state,actiontime) / 
+#       - check_time(data,actiontime) / comprare time in data to actual time
+#       - ec2instances_action(instanceslist,action) /  start or stop instances, based on ID instances list
 #
 #####################################################################################################################
 
@@ -95,4 +101,193 @@ def get_ec2tagsvalues(region,instanceslist,tagslist):
 
     return returnlist
 
+#####################################################################################################################
+#
+#####################################################################################################################
+#
+# Verify if the parameter is well formated for time rekognition (HH:MM:SS+HH:MM:SS)
+#
+# Input1 : data to analyse
+# Output : True (format ok) or False (format ko)
+#
+def verify_time_format(data):
+
+    import re
+
+    if re.match('^[0-9]{2}\:[0-9]{2}\:[0-9]{2}\+[0-9]{2}\:[0-9]{2}$',data):
+        if int(data[:2])==99:
+            return True
+        if (int(data[:2])>23) or (int(data[3:5])>59) or (int(data[6:8])>59):
+            print (data+" : Time Format OK - Data KO")
+            return False
+        else:
+            return True
+    else:
+        # print (data+" : Time Format KO")
+        return False
+
+#####################################################################################################################
+#
+#####################################################################################################################
+#
+# Verify if the parameter is well formated for date rekognition (MON,TUE,....)
+#
+# Input1 : data to analyse
+# Output : True (format ok) or False (format ko)
+#
+def verify_days_format(data):
+
+    import re
+
+    if re.match('^[aA-zZ]{3}(,[aA-zZ]{3})*$',data):
+        return True
+        print (data + " : OpeningDays Format KO")
+    else:
+        return False
+
+#####################################################################################################################
+#
+#####################################################################################################################
+#
+# Verify if in the parameter data, we can find a day similar to today
+#
+# Input1 : data to analyse (some days in three digit format, enclosed by "'")
+# Output : True (today is integrated in data) or False (today is not integrated in data)
+#
+def check_day(data):
+
+    from time import strftime
+    n = 0
+
+    actualday = strftime("%a").upper()
+
+    datasplit=data.split(",")
+    for day in datasplit:
+        if day == actualday:
+            n=1
+
+    if n == 1:
+        return True
+    else:
+        return False
+
+#####################################################################################################################
+#
+#####################################################################################################################
+#
+# Verify if in the parameter data, we can find a day similar to today
+#
+# Input1 : starttime of the AWS object
+# Input2 : stoptime of the AWS object
+# Input3 : actual state of the AWS object (running/stopped)
+# Output :  0 => do nothing
+#           1 => action
+#           2 => starttime and stoptime are the same but not 99
+#
+def check_slot(starttime,stoptime,state,actiontime):
+
+    #print ("StartTime : "+tagvalue1)
+    #print ("StopTime : "+tagvalue2)
+
+    # If StartTime = 99
+    #   If StopTime=99 => do nothing
+    #   Else
+    #       If state=running and StopTime<ActualTime => stop server
+    #       Else
+    #       Do nothing
+    # And If StopTime = 99
+    #   If StartTime=99 => Do nothing
+    #   Else
+    #       If State=stopped and StartTime<ActualTime => Start Server
+    #       Else
+    #       Do nothing
+    # And If StartTime<>99 and StopTime<>99 and StartTime=StopTime => 2
+    # Else
+    #   If State=stopped
+    #       If StartTime<Actualtime and StopTime>ActualTime => Start Server
+    #       Else => Do nothing
+    #   Else If State=running
+    #       If StartTime<ActualTime and StopTime>ActualTime or StartTime>ActuelTime and StopTime> ActualTime => Stop Server
+    #       Else => Do nothing
+    #   Else
+    #       => Do nothing
+
+    if int(tagvalue1[:2]) == 99:
+        if int(tagvalue2[:2]) == 99:
+            return 0
+        else:
+            if state == "running" and check_time(tagvalue2,actiontime) == 1:
+                return 1
+            else:
+                return 0
+    elif int(tagvalue2[:2]) == 99:
+        if int(tagvalue1[:2]) == 99:
+            return 0
+        else:
+            if state == "stopped" and check_time(tagvalue1,actiontime) == 1:
+                return 1
+            else:
+                return 0
+    elif (int(tagvalue1[:2]) != 99) and (int(tagvalue2[:2]) != 99) and (tagvalue1 == tagvalue2):
+        return 2
+    else:
+        if state == "stopped":
+            if check_time(tagvalue1,actiontime) == 1 and check_time(tagvalue2,actiontime) == 0:
+                return 1
+            else:
+                return 0
+        elif state == "running":
+            if (check_time(tagvalue1,actiontime) == 1 and check_time(tagvalue2,actiontime) == 1) or (check_time(tagvalue1,actiontime) == 0 and check_time(tagvalue2,actiontime) == 0):
+                return 1
+            else:
+                return 0
+        else:
+            return 0
+
+#####################################################################################################################
+#
+#####################################################################################################################
+#
+# Verify if time in parameter is in advance or late compared to the actual time
+#
+# Input1 : time
+# Output :  0 => time in parameter is greater than actual time
+#           1 => time in parameter is less or equal than actual time
+#
+def check_time(data,actiontime):
+
+    from datetime import time
+
+    actualtime = time(int(actiontime[:2]), int(actiontime[3:5]), int(actiontime[6:8]))
+    idgmttime = time(int(data[:2]), int(data[3:5]), int(data[6:8]))
+
+    if idgmttime <= actualtime:
+        return 1
+    else:
+        return 0
+
+#####################################################################################################################
+#
+#####################################################################################################################
+#
+# Action for Ec2 Instances
+#
+# Input1 : ID list of instances
+# Output : Nothing
+#
+def ec2instances_action(instanceslist,action):
+
+    ec2 = boto3.client('ec2', region_name=region)
+
+    if action == "start":
+        for id in instanceslist:
+            print (id+" starting")
+        #ec2.start_instances(InstanceIds=instanceslist)
+    else:
+        for id in instanceslist:
+            print (id+" stopping")
+        #ec2.stop_instances(InstanceIds=instanceslist)
+
+#####################################################################################################################
+# The End
 #####################################################################################################################
